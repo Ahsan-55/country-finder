7d93a779d25c959c5cdb039559bb6139075bf584
Muhammad Ahsan Bashir <116182912+Ahsan-55@users.noreply.github.com>
2022-12-29 17:33:58 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Countries from "./components/Countries";
+import React from 'react'
+import NavBar from "./components/NavBar";
+
+function App() {
+
+  
+
+  return (
+    <div className="container-fluid p-0">
+      <NavBar />
+      <Countries />
+    </div>
+  );
+}
+
+export default App;
